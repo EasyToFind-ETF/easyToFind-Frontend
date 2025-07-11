8697bed2b9922bf8ad6292ed2ff174d41a313828
slay <palgalow@gmail.com>
2025-07-11 14:45:45 +0900

    [feat]: 네브바 생성 및 이미지 파일 업로드

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { oneShinhan } from "@/styles/fonts";
 import "./globals.css";
+import { Header } from "@/components/layout/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${oneShinhan.variable} antialiased`}>{children}</body>
+      <body className={`${oneShinhan.variable} antialiased`}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,23 +1,23 @@
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
 export const oneShinhan = localFont({
   src: [
     {
-      path: '../../public/fonts/OneShinhan-Light.ttf',
-      weight: '300',
-      style: 'normal',
+      path: "../assets/fonts/ONESHINHANLIGHT 2.ttf",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/OneShinhan-Medium.ttf',
-      weight: '500',
-      style: 'normal',
+      path: "../assets/fonts/ONESHINHANMEDIUM 2.ttf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/OneShinhan-Bold.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "../assets/fonts/ONESHINHANBOLD 2.ttf",
+      weight: "700",
+      style: "normal",
     },
   ],
-  display: 'swap',
-  variable: '--font-one-shinhan',
+  display: "swap",
+  variable: "--font-one-shinhan",
 });
