2590dca7eaacce4364fd9c95780f08541fcf13bc
Dayoung331 <110375535+Dayoung331@users.noreply.github.com>
2025-07-17 08:13:37 +0900

    ETF 찾기 백엔드 부분 연동

---
+++ src/types/ETFView.ts
@@ -0,0 +1,12 @@
+export type ETFView = {
+    name: string;
+    nav: string;
+    week1: string;
+    month1: string;
+    month3: string;
+    month6: string;
+    year1: string;
+    year3: string;
+    ytd: string;
+    inception: string;
+}
